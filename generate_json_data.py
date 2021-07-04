e83f954dc15f0d12a00fa5be5a877d39b5f23fd0
Sankar <sanmuk21@gmail.com>
2021-07-04 15:27:56 +0530

    Added JSON dataset generator

--- generate_json_data.py
+++ generate_json_data.py
@@ -1,11 +1,11 @@
 """
-Generate random data with Faker and save as CSV. You can specify list of rows and type.
+Generate random data with Faker and save as JSON. You can specify list of rows and type.
 The data type should be valid provider from Faker library.
 Faker Library: https://faker.readthedocs.io/en/master/index.html
 
 """
 from faker import Faker
-import csv
+import json
 import os
 import logging
 import tempfile
@@ -14,12 +14,12 @@ logging.basicConfig(level = os.getenv("LOGLEVEL", "INFO"))
 log = logging.getLogger(__name__)
 
 # Specify data schema as column name and Faker type as tuple
-SCHEMA = [ ("name", "name"), ("company", "company"), ("phone", "phone_number") ]
+SCHEMA = [ ("name", "name"), ("city", "city"), ("salary", "random_int") ]
 TOTAL_RECORDS = 5
 OUTFILE_HOME = None
 OUTFILE_NAME = None
 OUTFILE_HOME = "/Users/apple/TEST/DataEngineeringPython/01"
-OUTFILE_NAME = "sample.csv"
+OUTFILE_NAME = "sample.json"
 
 def generate(schema=SCHEMA, total_rcords=TOTAL_RECORDS, output_file_name=OUTFILE_NAME, output_home=OUTFILE_HOME):
     """
@@ -30,31 +30,30 @@ def generate(schema=SCHEMA, total_rcords=TOTAL_RECORDS, output_file_name=OUTFILE
     :param output_home:
     :return: File name
     """
-    output_file = tempfile.NamedTemporaryFile().name + ".csv"
+    output_file = tempfile.NamedTemporaryFile().name + ".json"
     if output_file_name and output_home :
         output_file = os.path.join(output_home, output_file_name)
     if os.path.isfile(output_file):
         log.warning(f"File {output_file} already exists, thus deleting it.")
         os.remove(output_file)
 
-    log.info(f"Creating CSV data in file {output_file}.")
+    log.info(f"Creating JSON data in file {output_file}.")
     log.debug(f"Schema: {schema}.")
     log.debug(f"Total records: {total_rcords}.")
     f = Faker()
 
     try:
         fh = open(output_file, 'w')
-        csvw = csv.writer(fh)
-        hdr = [k for k, v in schema]
-        hdr.insert(0, "id")
-        log.debug(f"Header {hdr}")
-        csvw.writerow(hdr)
+        jsn = {}
+        jsn["data"] = []
         for i in range(total_rcords):
-            row = [i]
+            do = { "id": i }
             for k, v in schema:
-                row.append(f.__getattr__(v)())
-            log.debug(f"Row: {row}")
-            csvw.writerow(row)
+                do[k] = f.__getattr__(v)()
+            log.debug(f"Object: {do}")
+            jsn["data"].append(do)
+        log.debug(jsn)
+        json.dump(jsn, fh)
     except Exception as e:
         log.error("Operation failed")
         log.error(e)
@@ -64,33 +63,31 @@ def generate(schema=SCHEMA, total_rcords=TOTAL_RECORDS, output_file_name=OUTFILE
         fh.close()
     return output_file
 
-def read_csv(file_name):
+def read_json(file_name, schema=SCHEMA):
     """
-    Read a csv file
+    Read a JSON file
     :param file_name:
+    :param schema:
     :return: None
     """
 
     if not os.path.isfile(file_name):
         log.error(f"File {file_name} does not exist.")
 
+    log.info(f"Starting to read JSON file {file_name}.")
     fh = open(file_name)
-    fr = csv.DictReader(fh)
-    log.info(f"Starting to read csv file {file_name}.")
-    hdr = fr.fieldnames
-    log.info(f"Schema found: {hdr}")
-    for r in fr:
+    jd = json.load(fh)
+    for d in jd['data']:
         row = []
-        for i in range(len(hdr)):
-            c = hdr[i]
-            log.debug(f"{c}: {r[c]}")
-            row.append(f"{c}: {r[c]}")
-        log.info(row)
+        for se in schema:
+            row.append(d[se[0]])
+        log.info(f"Row: {row}")
+
 
 if __name__ == '__main__':
 
     file_nm = generate()
-    read_csv(file_nm)
+    read_json(file_nm)
 
 
 
